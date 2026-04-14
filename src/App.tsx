8561a5fda45bc230533e76d4761e77ceb96d19e4
alvin081105 <ca081105@gmail.com>
2026-04-14 12:22:21 +0900

    feat: configure routing with application pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
 import { Routes, Route } from 'react-router-dom'
+import GNB from './components/layout/GNB'
+import Footer from './components/layout/Footer'
 import HomePage from './pages/HomePage'
+import ProductListPage from './pages/ProductListPage'
+import ProductDetailPage from './pages/ProductDetailPage'
+import CartPage from './pages/CartPage'
+import CheckoutPage from './pages/CheckoutPage'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+import MyPage from './pages/MyPage'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-    </Routes>
+    <div className="min-h-screen flex flex-col">
+      <GNB />
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products" element={<ProductListPage />} />
+          <Route path="/products/:id" element={<ProductDetailPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/mypage" element={<MyPage />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   )
 }
 
